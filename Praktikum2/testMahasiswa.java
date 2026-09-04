9d27cb554c3ccd2d4603a48cc8b03f8c888edeed
wasik <wasikwwasikm@gmail.com>
2026-09-04 09:16:46 +0700

    tambahInisiasi

--- Praktikum2/testMahasiswa.java
+++ Praktikum2/testMahasiswa.java
@@ -8,5 +8,19 @@ public class testMahasiswa {
         mhs1.alamat = "Jl. Vinolia No 1A";
         mhs1.kelas = "1A";
         mhs1.tampilBiodata();
+
+        mahasiswa mhs2 = new mahasiswa();
+        mhs2.nim = 102;
+        mhs2.nama = "Rizky";
+        mhs2.alamat = "Jl. Vinolia No 1B";
+        mhs2.kelas = "1B";
+        mhs2.tampilBiodata();
+
+        mahasiswa mhs3 = new mahasiswa();
+        mhs3.nim = 103;
+        mhs3.nama = "Dewi";
+        mhs3.alamat = "Jl. Vinolia No 1C";
+        mhs3.kelas = "1C";
+        mhs3.tampilBiodata();
     }
 }
