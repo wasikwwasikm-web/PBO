368a2e084a63aa0695ac8333fdf41855803f2e56
wasik <wasikwwasikm@gmail.com>
2026-09-04 08:59:33 +0700

    percobaan2

---
+++ Praktikum2/mahasiswa.java
@@ -0,0 +1,15 @@
+package Praktikum2;
+
+public class mahasiswa {
+    public int nim;
+    public String nama;
+    public String alamat;
+    public String kelas;
+
+    public void tampilBiodata() {
+        System.out.println("NIM : " + nim);
+        System.out.println("Nama : " + nama);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Kelas : " + kelas);
+    }
+}
---
+++ Praktikum2/testMahasiswa.java
@@ -0,0 +1,12 @@
+package Praktikum2;
+
+public class testMahasiswa {
+    public static void main(String[] args) {
+        mahasiswa mhs1 = new mahasiswa();
+        mhs1.nim = 101;
+        mhs1.nama = "Lestari";
+        mhs1.alamat = "Jl. Vinolia No 1A";
+        mhs1.kelas = "1A";
+        mhs1.tampilBiodata();
+    }
+}
