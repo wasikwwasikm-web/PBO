3bf1a70abdab56e38f9781e65be15120be9e186f
wasik <wasikwwasikm@gmail.com>
2026-09-11 10:05:02 +0700

    percobaan3

---
+++ MotorEncapsulation/koperasiDemo.java
@@ -0,0 +1,14 @@
+package MotorEncapsulation;
+
+public class koperasiDemo {
+    public static void main(String[] args) {
+        anggota anggota1 = new anggota();
+        anggota1.setNama("Iwan Setiawan");
+        anggota1.setAlamat("Jl.Sukarno Hatta No. 10");
+        anggota1.setor(100000);
+        System.out.println("Simpanan " + anggota1.getNama() + " : " + anggota1.getSimpanan());
+
+        anggota1.pinjam(5000);
+        System.out.println("Simpanan " + anggota1.getNama() + " : " + anggota1.getSimpanan());
+    }
+}
