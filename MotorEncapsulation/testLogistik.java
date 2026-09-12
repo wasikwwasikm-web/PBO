73cb0cb1cefeac09f4da9ba80e1627227454019c
wasik <wasikwwasikm@gmail.com>
2026-09-12 14:29:36 +0700

    modNo6

--- MotorEncapsulation/testLogistik.java
+++ MotorEncapsulation/testLogistik.java
@@ -1,23 +1,23 @@
 package MotorEncapsulation;
 
+import java.util.Scanner;
+
 public class testLogistik {
-
     public static void main(String[] args) {
-        kontainer kontainerAlfa = new kontainer("REQ-9988", "PT. Maju Bersama", 10000);
+        Scanner sc = new Scanner(System.in);
+        kontainer kontainerAlfa = new kontainer("REQ-9988", "PT. Maju Bersama", 5000);
 
-        System.out.println("Nama Pemilik Kontainer:  " + kontainerAlfa.getNamaPemilik());
-        System.out.println("Kapasitas Kontainer: " + kontainerAlfa.getKapasitasMaksimal() + " kg");
+        System.out.println("Nama Pemilik Kontainer: " + kontainerAlfa.getNamaPemilik());
+        System.out.println("Kapasitas Maksimal: " + kontainerAlfa.getKapasitasMaksimal() + " kg");
 
-        System.out.println("\nMasukkan muatan baru seberat 6000 kg...");
-        kontainerAlfa.tambahMuatan(6000);
+        System.out.print("\nMasukkan berat muatan yang ingin ditambahkan (kg): ");
+        int tambah1 = sc.nextInt();
+        kontainerAlfa.tambahMuatan(tambah1);
         System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
 
-        System.out.println("\nMasukkan muatan baru seberat 4000 kg...");
-        kontainerAlfa.tambahMuatan(4000);
-        System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
-
-        System.out.println("\nMembongkar muatan seberat 6000 kg...");
-        kontainerAlfa.turunkanMuatan(6000);
+        System.out.print("\nMasukkan berat muatan yang ingin dibongkar (kg): ");
+        int bongkar1 = sc.nextInt();
+        kontainerAlfa.turunkanMuatan(bongkar1);
         System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
     }
 }
